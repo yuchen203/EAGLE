8c7f40233c70f1c1ff440c3f822dde3679d60981
yuchen203 <79148954+yuchen203@users.noreply.github.com>
2025-12-01 23:42:45 -0500

    update test

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
 import torch, json, time
-from datasets import load_dataset
 from eagle.model.ea_model import EaModel
 from fastchat.model import get_conversation_template 
 
-dataset = load_dataset("mbpp", "full")["test"]
-
+test_name = "gsm8k"
 model = EaModel.from_pretrained(
     base_model_path="../Qwen3-8B",
     ea_model_path="../qwen3_8b_eagle3",
@@ -13,46 +11,49 @@ model = EaModel.from_pretrained(
     device_map="auto",
     total_token=-1
 )
-
 def run_one_question(question):
     # tokenizer encode to ids
     input_ids = model.tokenizer([question]).input_ids
     input_ids = torch.as_tensor(input_ids).cuda()
-
+    start = torch.cuda.Event(enable_timing=True)
+    end = torch.cuda.Event(enable_timing=True)
     # generate
-    start = time.perf_counter()
+    start.record()
     output_ids = model.eagenerate(
         input_ids,
         temperature=0.0,
         max_new_tokens=512
     )
-    end = time.perf_counter()
-    duration = end - start
+    end.record()
+    torch.cuda.synchronize()
+    duration = start.elapsed_time(end) / 1000.0
     output = model.tokenizer.decode(output_ids[0])
     return output, duration
 
 output_list = []
 duration_list = []
+total_examples = 100
 
-for i  in range(11, 111):
-    for item in dataset:
-        if item['task_id'] == i:
-            output, duration =run_one_question(item["text"])
-            print("finished example", item['task_id'])
-            output_list.append(output)
-            duration_list.append(duration)
+with open(f"{test_name}_input.json", "r", encoding="utf-8") as f:
+    dataset = json.load(f)
+for i in range(total_examples):
+    output, duration =run_one_question(dataset[str(i)])
+    print("finished example", i)
+    output_list.append(output)
+    duration_list.append(duration)
 
 result = {}
-result["dateset"] = "mbpp"
+result["dateset"] = test_name
 result["model"] = "qwen3_8b_eagle3"
-result["total_examples"] = 100
+result["total_examples"] = total_examples
 result["generated_texts"] = []
-for i in range(100):
+for i in range(total_examples):
     result["generated_texts"].append({
-        "example_id": i + 11,
+        "example_id": i,
+        "input_text": dataset[str(i)],
         "full_text": output_list[i]
     })
 result["times"] = duration_list
 
-with open("mbpp_eagle3.json", "w", encoding="utf-8") as f:
-    json.dump(result, f, ensure_ascii=False, indent=2)
+with open(f"{test_name}_eagle3.json", "w", encoding="utf-8") as f:
+    json.dump(result, f, ensure_ascii=False, indent=4)
